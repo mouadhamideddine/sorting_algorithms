fcd4c790fdf90d9cc425362eb64e8f371865e1a4
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2024-02-21 00:48:52 +0100

    done

--- sort.h
+++ sort.h
@@ -1,7 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 /*SORT.H file*/
-
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -12,9 +12,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
